b547a98ebd0e33bc93b7ad807323c0192a5a2a7d
Matt Chastain <work@mattchastain.dev>
2025-03-04 20:16:22 -0500

    add currentWeather to slice

--- src/store/locationSlice.js
+++ src/store/locationSlice.js
@@ -3,6 +3,7 @@ import { getWeatherData } from '../api/weather';
 
 const initialState = {
 	location: { name: 'Nashville, Tennessee', lat: 36.1659, lon: -86.7844 },
+	currentWeather: {},
 	weatherData: {},
 	error: false,
 	isLoading: false,
@@ -21,7 +22,8 @@ const locationSlice = createSlice({
 		},
 		getWeatherDataSuccess: (state, action) => {
 			state.isLoading = false;
-			state.weatherData = action.payload;
+			state.currentWeather = action.payload.currentWeather;
+			state.weatherData = action.payload.forecast;
 		},
 		getWeatherDataFailed: (state) => {
 			state.isLoading = false;
@@ -41,15 +43,18 @@ export default locationSlice.reducer;
 export const fetchWeatherData = (location) => async (dispatch) => {
 	try {
 		dispatch(startGetWeatherData());
-		const data = await getWeatherData(location.lat, location.lon);
-		const combinedData = data.time.map((time, index) => ({
+		const { currentWeather, forecast } = await getWeatherData(
+			location.lat,
+			location.lon
+		);
+		const combinedData = forecast.time.map((time, index) => ({
 			time,
-			temperature_2m_max: data.temperature_2m_max[index],
-			temperature_2m_min: data.temperature_2m_min[index],
-			weathercode: data.weathercode[index],
-			wind_speed_10m_max: data.wind_speed_10m_max[index],
+			temperature_2m_max: forecast.temperature_2m_max[index],
+			temperature_2m_min: forecast.temperature_2m_min[index],
+			weathercode: forecast.weathercode[index],
+			wind_speed_10m_max: forecast.wind_speed_10m_max[index],
 		}));
-		dispatch(getWeatherDataSuccess(combinedData));
+		dispatch(getWeatherDataSuccess({ currentWeather: currentWeather, forecast: combinedData }));
 	} catch (err) {
 		dispatch(getWeatherDataFailed());
 	}
